72adbcd2e18b2aa4c198921f1a9c72bfd4a40504
Leonardo Quenta <lquenta@gmail.com>
2014-08-08 12:46:55 -0400

    Fix reporte de deuda

--- app/Controller/TestsController.php
+++ app/Controller/TestsController.php
@@ -22,6 +22,23 @@ class TestsController extends AppController
 
         } 
 
+        function TestReporteDeuda(){
+            $this->layout = 'pdf';
+            $parametrosReporteDeuda=array(                    
+                'nombrecliente_uno'=>'Jose Carrasco',
+                'direccion'=>'Otero de la Vega 343',
+                 'ciudad'=>'La Paz',
+                 'total_deuda'=>400,
+                 'nombrecliente_dos'=>'Margarita Loza',
+                'direccion_dos'=>'Rios Perez 383',
+                 'ciudad_dos'=>'Cochabamba',
+                 'total_deudados'=>5000,
+                 );      
+            $this->set('parametrosReporteDeuda',$parametrosReporteDeuda);
+            
+            $this->render();
+        }
+
 
     } 
 ?>
